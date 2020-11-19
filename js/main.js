bb28f6c32c94ae6a5925340ea8ca38db29916c81
Julian Kizeno <julianquiceno20@gmail.com>
2020-11-19 17:37:29 +0100

    class Game created

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+const celeste = document.getElementById('celeste')
+const violet = document.getElementById('violet')
+const oranje = document.getElementById('oranje')
+const green = document.getElementById('green')
+const yellow = document.getElementById('yellow')
+const blue = document.getElementById('blue')
+const red = document.getElementById('red')
+const grey = document.getElementById('grey')
+const salmon = document.getElementById('salmon')
+const pink = document.getElementById('pink')
+const burlywood = document.getElementById('burlywood')
+const gold = document.getElementById('gold')
+
+
+class Game {
+    constructor() {
+        this.starting()
+    }
+
+    starting() {
+        
+    }
+
+}
+
+function startTheGame(){
+    let game = new Game()
+}
